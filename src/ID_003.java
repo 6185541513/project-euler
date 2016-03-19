dfd01dc75bea4cbb3c9081d68359b7da19664887
Beatrice Wittgren <6185541513@protonmail.com>
2016-03-19 16:16:40 +0100

    added PrimeNumber class. scribbled in ID_003

--- src/ID_003.java
+++ src/ID_003.java
@@ -1,9 +1,31 @@
+import java.util.ArrayList;
+
 
 public class ID_003 implements Problem{
-
-	public void solve() {
-		// TODO Auto-generated method stub
+	private final long n = 600851475143L;
+	private final int t = 13195;
+	private ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+	
+	private long findLargestPrimeFactor() {
+		for(int i = 0; i <= t; i++) {
+			if(isPrimeFactor(i)) {
+				primeFactors.add(i);
+			}
+		}
+		return 0;
+	}
+	
+	private boolean isPrimeFactor(int n) {
 		
+		return false;
+	}
+	
+	private boolean isInteger(int n) {
+		return n == (int)n ? true : false;
+	}
+	
+	public void solve() {
+		System.out.println(findLargestPrimeFactor());
 	}//solve
 
 }
