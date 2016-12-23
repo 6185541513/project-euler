54c154ed1c85470582ff03492e4b6f17eceff33f
6185541513 <6185541513@protonmail.com>
2016-12-23 18:12:51 +0100

    made first draft of solution for ID_005

--- src/ID_005.java
+++ src/ID_005.java
@@ -1,5 +1,35 @@
 
 public class ID_005 implements Problem{
 
+    private final int RANGE_MIN = 1;
+    private final int RANGE_MAX = 20;
     
+    private int smallestMultiple(){
+	int smallestMultiple = 2520;
+	boolean keepGoing = true;
+
+	while(keepGoing) {
+	    for(int i = RANGE_MAX; i >= RANGE_MIN; i--){
+		//if i not evenly devicible with smallestMultiple, break loop
+		if(!isEvenlyDevisible(i)){
+		    break;
+		}
+		//else if i is evenly devisible and is value of RANGE_MIN, answer is found
+		else if(i == RANGE_MIN && isEvenlyDevisible(i)){
+		    return smallestMultiple;
+		}
+	    }//for
+	    ++smallestMultiple;
+	}//while
+
+	return -1;
+    }
+
+    private boolean isEvenlyDevisible(int value){
+	return (smallestMultiple % value == 0);
+    }
+    
+    public void solve(){
+	System.out.println("smallest multiple: " + smallestMultiple());
+    }
 }
