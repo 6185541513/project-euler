9d23e0e7d0b2e5723703f910d3b3a84d2662c588
6185541513 <6185541513@protonmail.com>
2016-12-25 21:14:09 +0100

    updated ID_007 with Prime. showed faster execution.

--- src/ID_007.java
+++ src/ID_007.java
@@ -1,31 +1,21 @@
 
 public class ID_007 implements Problem {
+    //methods are specialized rather than general, since there is no
+    //foreseen need for the latter regarding finding prime of certain index
 
-    private final int PRIME_MIN = 2;
     private final int INDEX_X = 10001;
     
     private int primeOfIndexX() {
-	int prime = 3;//start on first uneven prime (2nd prime in order)
-	boolean keepGoing;
+	int n = 3;//start on first uneven prime (2nd prime in order)
 
 	for(int i = 3; i <= INDEX_X; i++) {//start on index after first uneven prime
-	    prime += 2;
-	    while(!isPrime(prime)){
-		prime += 2;
+	    n += 2;
+	    while(!Prime.meetsCondition(n)){
+		n += 2;
 	    }//while
 	}//for
 	
-	return prime;
-    }
-
-    private boolean isPrime(int n) {
-	for(int i = 2; i < n; i++){
-	    if(n % i == 0) {
-		return false;
-	    }
-	}
-
-	return true;
+	return n;
     }
     
     public void solve() {
