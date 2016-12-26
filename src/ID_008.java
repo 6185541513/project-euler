2afc4a34d495e76168117c0d387e05d7c47db01d
6185541513 <6185541513@protonmail.com>
2016-12-26 12:36:02 +0100

    added working solution for ID_008

--- src/ID_008.java
+++ src/ID_008.java
@@ -1,50 +1,70 @@
 
+import java.io.*;
+import static java.nio.file.StandardOpenOption.*;
+import java.nio.*;
+import java.nio.file.*;
+
 public class ID_008 implements Problem {
 
-    private final int SIZE_OF_ADJACENT = 4;
-    int[] adjacent = new int[SIZE_OF_ADJACENT];
+    private final int SIZE = 13;
     
-    private int largestProductInSeries() {
-	Scanner scanner = new Scanner(new File("integer_series.txt"));
-	int i = 0;
-	int largestProduct = 0;
-	
-	//fill adjacent arr (unchecked since has necessary info)
-	for(i < SIZE_OF_ADJACENT; i++){
-	    adjacent[i] = scanner.nextInt();
-	    largestProduct = adjacent[i];
-	}//for
-	
-	while(scanner.hasNextInt()) {
-	    if(scanner.nextInt() > getLargest()) {
-		
-	    }
+    private long largestProductInSeries() {
+	int[] largest = new int[SIZE];//largest adjacent arr
+	int[] curr = new int[SIZE];//current adjacent arr
+	try {
+	    Reader reader = Files.newBufferedReader(Paths.get("integer_series.txt"));
 	    
-	}//while
+	    //fill adjacent arr (unchecked since has necessary info)
+	    for(int i = 0; i < SIZE; i++){
+		largest[i] = Character.getNumericValue((char)reader.read());
+	    }//for
+	    curr = largest.clone();
 
-	return largestProduct;
+	    int c;
+	    while((c = reader.read()) != -1) {
+		char ch = (char)c;
+		int nextInt = Character.getNumericValue(ch);
+
+		//sanity check to ignore new line
+		if(nextInt >= 0){
+		    //check if updated curr arr is greater than, or
+		    //equal to, largest arr
+		    if(getProduct(updateArr(curr, nextInt)) >= getProduct(largest)) {
+			largest = curr.clone();
+		    }//if
+		}//if
+	    
+	    }//while
+	    
+	} catch(IOException e) {
+	    e.printStackTrace();
+	}
+
+	return getProduct(largest);
     }
 
-    private int getLargest() {
-	int largest = 0;
-	for(int i = 0; i < SIZE_OF_ADJACENT; i++) {
-	    if(adjacent[i] > largest) {
-		largest = adjacent[i];
-	    }
-	}//for
-
-	return largest;
+    private long getProduct(int[] arr) {
+	long product = 1L;
+	for(int i = 0; i < SIZE; i++) {
+	    product *= arr[i];
+	}
+	return product;
     }
 
-    //unchecked because reasons
-    private int getIndex(int n) {
-	for(int i = 0; i < SIZE_OF_ADJACENT; i++) {
-	    if(adjacent[i] == n) {
-		return i;
-	    }
-	}//for
-	
-	return -1;
+    private int[] updateArr(int[] arr, int n) {
+	for(int i = 0; i < SIZE-1; i++) {
+	    arr[i] = arr[i + 1];
+	}
+	arr[SIZE - 1] = n;
+	return arr;
+    }
+
+    //debug
+    private void printArr(int[] arr) {
+	for(int i = 0; i < SIZE; i++) {
+	    System.out.print(arr[i]);
+	}
+	System.out.println(" : " + getProduct(arr));
     }
     
     public void solve() {
--- src/ProjectEuler.java
+++ src/ProjectEuler.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 public class ProjectEuler {
 	
 	public static void main(String [] args) {
-		Problem problem = new ID_007();
+		Problem problem = new ID_008();
 		problem.solve();
-		System.out.println("1051 is prime: " + Prime.meetsCondition(1051) +
-				   ", but 1052 is not: " + Prime.meetsCondition(1052));
 	}//main
 	
 }
